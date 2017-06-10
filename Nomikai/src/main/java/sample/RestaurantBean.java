eb6ea9b6355aa99f8c56e67a8ed1fd2b6f38c4a3
Hiroki Sawano <hiroki.sawano.2512@gmail.com>
2017-06-10 19:34:31 +0900

    first commit

---
+++ Nomikai/src/main/java/sample/RestaurantBean.java
@@ -0,0 +1,40 @@
+package sample;
+
+import common.Restaurant;
+import common.RestaurantFacade;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class RestaurantBean implements Serializable {
+
+    @EJB
+    private RestaurantFacade rf;
+    
+    private List<Restaurant> restaurants;
+    
+    public List<Restaurant> getRestaurants(){
+        return restaurants;
+    }
+    
+    public String showRestaurants() {
+        // sample data
+        /*Restaurant r1 = new Restaurant();
+        r1.setId(1);
+        r1.setName("AAA");
+        rf.create(r1);
+        Restaurant r2 = new Restaurant();
+        r2.setId(2);
+        r2.setName("BBB");
+        rf.create(r2);*/
+        
+        restaurants = rf.findAll();
+        
+        return "restaurant.xhtml";
+    }
+}
